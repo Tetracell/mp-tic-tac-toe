8018ceacc3c6003f0eac057dd68287f08a936495
Tom LaTulipe <tlatulipe@gmail.com>
2024-07-23 21:28:59 -0400

    user registration inputs

---
+++ client/src/components/SignUp.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+function SignUp() {
+  const [user, setUser] = useState(null);
+  return (
+    <div className="signUp">
+      <label>Sign Up!</label>
+      <input
+        placeholder="First Name"
+        onChange={(event) => {
+          setUser({ ...user, firstName: event.target.value });
+        }}
+      />
+      <input
+        placeholder="Last Name"
+        onChange={(event) => {
+          setUser({ ...user, lastName: event.target.value });
+        }}
+      />
+      <input
+        placeholder="Password"
+        onChange={(event) => {
+          setUser({ ...user, password: event.target.value });
+        }}
+      />
+    </div>
+  );
+}
+
+export default SignUp;
